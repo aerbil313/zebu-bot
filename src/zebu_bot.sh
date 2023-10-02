26ce21b928ecbca2c32eeaee0d5978d7c1ad4b5a
aerbil313 <aerenbilir@gmail.com>
2023-10-02 13:33:38 +0300

    remove zebu-bot.sh

--- src/zebu_bot.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i fish --pure
-#! nix-shell -I https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz
-#! nix-shell --packages fish git cacert
-
