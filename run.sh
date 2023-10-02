eaca0bd0cc2df2a43e44d431e5550b9e00e5b985
aerbil313 <aerenbilir@gmail.com>
2023-10-02 08:49:21 +0300

    added install method

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i fish --pure
+#! nix-shell -I https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz
+#! nix-shell --packages fish git cacert python3
+
+git clone git@github.com:aerbil313/zebu-bot.git
+cd zebu-bot
+python3 -m venv .
+source bin/activate.fish
+pip install -r requirements.txt
+python3 src/main.py
