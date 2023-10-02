0ea83a2777ecbf1d73fb619ca47deb06ae18aa6a
aerbil313 <aerenbilir@gmail.com>
2023-10-02 17:01:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i fish --pure
 #! nix-shell -I https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz
-#! nix-shell --packages fish cacert python3
+#! nix-shell --packages fish cacert python311
 
 cd zebu-bot
 python3 -m venv .
